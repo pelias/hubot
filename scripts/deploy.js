6fe6fec6d4292d6b44a9f374ff71bd32ae0a4dfb
Julian Simioni <julian@simioni.org>
2016-01-06 14:35:41 -0500

    Remove test code to repeat everything

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,2 @@
-
 module.exports = function (robot) {
-
-  robot.hear(new RegExp('[a-z]*'), function (res) {
-    console.log(res.message);
-    res.send('I heard you say ' + res.message.text);
-  });
-
 };
